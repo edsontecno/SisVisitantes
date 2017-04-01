bdc2cb89c0683b6e27cbb7bafb163c3f7f2772cb
Edson <Edson@EDSON-SAMSUNG>
2017-03-31 22:30:02 -0300

    Adicionando carga nas tabelas

--- src/main/resources/sql/CRIAR USUARIO.sql
+++ src/main/resources/sql/CRIAR USUARIO.sql
@@ -27,3 +27,11 @@ CREATE TABLE perfil_permissao (
     FOREIGN KEY (id_permissao) REFERENCES permissao(id_permissao),
     FOREIGN KEY (id_perfil) REFERENCES perfil(id_perfil)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+--SENHA É TESTE123
+insert into `perfil` (`id_perfil`, `nome`) values('1','Administrador');
+insert into `permissao` (`id_permissao`, `nome`) values('1','CADASTRAR_USUARIO');
+insert into `perfil_permissao` (`id_permissao`, `id_perfil`) values('1','1');
+insert into `usuario` (`id_usuario`, `nome`, `email`, `senha`, `ativo`, `data_nascimento`, `id_perfil`) values('1','Edson','edsontecno@gmail.com','$2a$10$bWmqoFDMm33tNT30uy84fugTPPqOFUr39wUMAXEpgRSaKPV7GrK16','1',NULL,'1');
+
